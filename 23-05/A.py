6f6fe1169e461365fca6f9670945bb4b6e4ffc44
Raja <kelaya5@gmail.com>
2023-05-23 20:29:15 +0530

    commit

--- 23-05/A.py
+++ 23-05/A.py
@@ -3,9 +3,43 @@ print("hello 10");
 print(10);
 print(10);
 print("==================")
-id=10;
+s_id=10;
+s_id1=10;
+s_marks=100;
 
-print(id)
-print(id)
-print(id)
-print(id)
+print(s_id)
+print(s_marks)
+
+print(id(s_id))
+print(id(s_id1))
+print(id(s_marks))
+
+name="java";
+name1="java";
+print(id(name))
+print(id(name1))
+
+#de1vlopers communication
+
+'''
+multi line commands
+Author : Elayaraja
+Date : 23-05-2023
+Developed by:
+implemented by:
+Last modification
+'''
+
+Name =10;#valid
+_Name1="raja"#valid
+#123name="raja"; invalid
+#%names="dsfdsa"; special symbols are not allowed for identifier except _
+Name123="java";#valid
+
+Name="Raja";
+Intrusted="Programming";
+Hobbies="Studying";
+print("My name ")
+print(name)
+print(Intrusted)
+print(Hobbies);
