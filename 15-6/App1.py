061e0f415a37950ddfb29a77bdf2d6608c442974
Raja <kelaya5@gmail.com>
2023-06-15 20:17:52 +0530

    commit

---
+++ 15-6/App1.py
@@ -0,0 +1,44 @@
+# for and while
+
+#loop until condition is false it is keep on execute
+value=0;
+'''
+while(True):
+    print(value)
+    value = value +  1;
+'''
+
+
+'''
+for val in  range(5,10):
+    print(val)
+'''
+'''
+if(True):
+    if(True):
+        if(False):
+            print("Hello ")
+        elif(False):
+            print("Hello 1")
+        else:
+            if(False):
+                print("inner iif")
+            elif(False):
+                print("innner elif")
+            else:
+                print("from else")
+else:
+    print("From outer else")
+
+'''
+
+'''pattern program'''
+for x in range(6):
+    for y in range(x):      #nested for loop
+        print(y,end="");
+    print("");
+
+'''
+1st x=0
+   
+'''
